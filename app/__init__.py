da23da8c4a0dea8813ea0e723afd8c43a7436600
Master <mateluci_pl@hotmail.com>
2022-01-13 12:28:33 -0300

    Alteração DB

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_login import LoginManager
 from datetime import timedelta
 
 app = Flask(__name__, template_folder='./templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:99706855@localhost/flask_login'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:99706855@localhost/flask_login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://idexawu7uo5e90zg:iqpikux52el4bwu2@uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uo2l4tehiflllwwl'
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=10)
