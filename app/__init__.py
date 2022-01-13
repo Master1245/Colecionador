5b0dcdd87862330ade7bac28d66b165faa6d610b
Master <mateluci_pl@hotmail.com>
2022-01-13 12:34:42 -0300

    Esconder info bd

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from datetime import timedelta
+from dotenv import load_dotenv, find_dotenv
+import os 
+
+load_dotenv(find_dotenv())
+DB_NUVEM = os.environ.get('DB_NUVEM')
+DB_LOCAL = os.environ.get('DB_LOCAL')
 
 app = Flask(__name__, template_folder='./templates')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:99706855@localhost/flask_login'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://idexawu7uo5e90zg:iqpikux52el4bwu2@uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uo2l4tehiflllwwl'
+# app.config['SQLALCHEMY_DATABASE_URI'] = DB_LOCAL
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_NUVEM
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=10)
