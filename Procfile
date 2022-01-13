6790128ccd9f1de772c633c4e3360ba23149c1f6
Master <mateluci_pl@hotmail.com>
2022-01-13 15:50:55 -0300

    Adição de arquivos para heroku e add favicon

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:main.py
+web: gunicorn -b :$PORT app:app
--- main.py
+++ main.py
@@ -181,5 +181,6 @@ def register_type():
 
     return render_template('register_type.html', message="Favor preencher todos os campos")
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
