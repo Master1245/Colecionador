c5643127e17dced1102e7c2f6229e79ecc26fdd2
Oicanji <51388779+Oicanji@users.noreply.github.com>
2022-01-14 20:02:17 -0300

    .

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def home():
         return render_template('home.html', type=Item_type.query.all(), collection=collections) 
     else:
         # return redirect(url_for('login', message="Favor preencher todos os campos"))
-        return render_template('home.html', content="<h1>You need login</h1>")
+        return render_template('template.html', content="<h1>You need login</h1>")
         
 @app.route('/register' ,methods=['GET','POST'])
 def register():
