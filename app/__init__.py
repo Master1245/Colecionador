6429f14c736110717f0bbdce5b11e5d75114c7cd
Mateus <=>
2023-02-27 20:49:22 -0300

    Teste

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ BD_NUVEM = os.environ.get('BD_NUVEM')
 
 app = Flask(__name__, template_folder='../static/src' , static_folder='../static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = BD_NUVEM
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/colecionador'
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=60)
